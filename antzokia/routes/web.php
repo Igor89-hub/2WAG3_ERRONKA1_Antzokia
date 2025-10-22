e00ef4525a7004f1472da55f2b58c61137ee154c
Igor89-hub <igorviyusan@gmail.com>
2025-10-22 10:15:55 +0200

    Datubase eguneratuta eta Laravel konfiguratuta

--- antzokia/routes/web.php
+++ antzokia/routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welco
-    me');
+    return Inertia::render('welcome');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
