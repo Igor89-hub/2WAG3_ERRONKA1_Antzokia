1c9ec1d0a222535478d9816ad53637b95a4a2928
Igor89-hub <igorviyusan@gmail.com>
2025-10-22 12:00:20 +0200

    Leher orria egiten

---
+++ antzokia/resources/js/pages/hasiera.tsx
@@ -0,0 +1,17 @@
+export default function Hasiera() {
+    return(
+        <>
+        <div className="a-main">
+            <header className="a-main-header">
+                <div className="a-main-headerTopBar">
+                    <img className="a-main-headerTopBar-logo" alt="TEATRO LOGOA"/>
+                </div>
+                <p className="a-main-headerTopBar-SidebarButton">HamburgerLogo</p>
+            </header>
+            <div className="a-main-aurrenekoEkitaldia">
+                <h1>KAIXOO</h1>
+            </div>
+        </div>
+        </>
+    )
+}
