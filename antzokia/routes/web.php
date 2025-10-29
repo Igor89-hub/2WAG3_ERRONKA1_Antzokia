340a62a68c9438c46981fa509738b7abe7ea059e
Igor89-hub <igorviyusan@gmail.com>
2025-10-29 10:15:56 +0100

    cambios menores

--- antzokia/routes/web.php
+++ antzokia/routes/web.php
@@ -8,11 +8,11 @@ use Inertia\Inertia;
     return Inertia::render('hasiera');
 })->name('home');*/
 
-Route::middleware(['auth', 'verified'])->group(function () {
+/*Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-});
+});*/
 
 Route::get('/', [HasieraOrriaController::class, 'ekitaldiak'])->name('hasiera');
 
