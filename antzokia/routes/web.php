26491c4124e9256e39359aba74495b6701b2d98b
Igor89-hub <igorviyusan@gmail.com>
2025-11-05 11:56:38 +0100

    ekitaldiak editatu bukatzen

--- antzokia/routes/web.php
+++ antzokia/routes/web.php
@@ -31,6 +31,7 @@ Route::get('/Users/register', [RegisterController::class, 'openRegister'])->name
 
 //POST KONTSULTAK
 Route::post('/ekitaldiak', [EkitaldiakController::class, 'store'])->name('ekitaldiak.store');
+Route::put('/ekitaldiak/{ekitaldia}', [EkitaldiakController::class, 'update'])->name('ekitaldiak.update');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
