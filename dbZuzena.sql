d3f52e57377a5d3a7a7c0fff8375c7a4a6858d65
Igor89-hub <igorviyusan@gmail.com>
2025-11-12 18:47:28 +0100

    Datu base berria hedapenerako

--- dbZuzena.sql
+++ dbZuzena.sql
@@ -64,6 +64,34 @@ LOCK TABLES `cache_locks` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ekitaldi_eserleku`
+--
+
+DROP TABLE IF EXISTS `ekitaldi_eserleku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ekitaldi_eserleku` (
+  `id_eserleku` int NOT NULL,
+  `id_ekitaldia` int NOT NULL,
+  `hartuta` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_eserleku`,`id_ekitaldia`),
+  KEY `id_ekitaldia` (`id_ekitaldia`),
+  CONSTRAINT `ekitaldi_eserleku_ibfk_1` FOREIGN KEY (`id_eserleku`) REFERENCES `eserleku` (`id_eserleku`),
+  CONSTRAINT `ekitaldi_eserleku_ibfk_2` FOREIGN KEY (`id_ekitaldia`) REFERENCES `ekitaldiak` (`id_ekitaldia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ekitaldi_eserleku`
+--
+
+LOCK TABLES `ekitaldi_eserleku` WRITE;
+/*!40000 ALTER TABLE `ekitaldi_eserleku` DISABLE KEYS */;
+INSERT INTO `ekitaldi_eserleku` VALUES (1,3,0),(1,10,0),(2,3,0),(2,10,0),(3,3,0),(3,10,0),(4,3,0),(4,10,0),(5,3,0),(5,10,0);
+/*!40000 ALTER TABLE `ekitaldi_eserleku` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ekitaldiak`
 --
 
@@ -72,13 +100,14 @@ DROP TABLE IF EXISTS `ekitaldiak`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ekitaldiak` (
   `id_ekitaldia` int NOT NULL AUTO_INCREMENT,
-  `izena` varchar(40) DEFAULT NULL,
-  `noiz` varchar(40) DEFAULT NULL,
-  `ordua` datetime DEFAULT NULL,
-  `lekua` varchar(50) DEFAULT NULL,
-  `deskribapena` varchar(500) DEFAULT NULL,
+  `izena` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hasiera_data` datetime DEFAULT NULL,
+  `bukaera_data` datetime DEFAULT NULL,
+  `lekua` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deskribapena` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_ekitaldia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,6 +116,7 @@ CREATE TABLE `ekitaldiak` (
 
 LOCK TABLES `ekitaldiak` WRITE;
 /*!40000 ALTER TABLE `ekitaldiak` DISABLE KEYS */;
+INSERT INTO `ekitaldiak` VALUES (3,'LOS CALVOS','2025-12-13 19:00:00','2025-12-13 20:00:00','Teatro Arriaga Antzokia','Taller','/storage/ekitaldi_irudiak/y3Qqbr9axsYrzvHiypD2LMiiQ5j054m6WIbPWm4U.jpg'),(4,'LOS PAYASOS','2026-01-01 17:00:00','2025-01-01 20:30:00','Teatro Arriaga Antzokia','Funcion de payasos','/storage/ekitaldi_irudiak/ji7PDaGjwH0qrlMJY2iUpF6byNKCLoYw07KpUj82.jpg'),(5,'DEIMOS\'S LIFE','2026-01-02 10:20:00','2026-01-02 11:30:00','Teatro Arriaga Antzokia','Taller','/storage/ekitaldi_irudiak/IicOmS84I0sahee9rbHMhXIhKGy60Pfn5WeXBx3B.png'),(6,'DEIMOS V2','2025-11-28 20:00:00','2025-11-28 22:00:00','Teatro Aguinaga','LA VIDA DE DEIMOS SEGUNDA PARTE','/storage/ekitaldi_irudiak/fhhu3EYaKjvoDobquUYVkz35QX9NayDNBBAo41Bs.jpg'),(7,'Vals','2026-02-18 10:00:00','2026-02-18 11:30:00','Teatro Aguinaga','Baile de Valls','/storage/ekitaldi_irudiak/wPQqa0219DL2LJdTZDiU0Bcn4IuppE5Ln7O8hOsx.jpg'),(8,'EL PAYASO TRISTON','2025-11-14 12:30:00','2025-11-04 13:30:00','TATRO ARRIAGA','UNA DECEPCION DE TXISTE','/storage/ekitaldi_irudiak/hd0BHh60nLzYT8jaGLINWOeeOC4T1oyYDN8cXKUl.jpg'),(10,'CISNE DEL SOL','2025-12-23 18:30:00','2025-12-23 19:30:00','Teatro de Aitzaga','Una obra de teatro de un ciste','/storage/ekitaldi_irudiak/rzcpqNSWN2V5ckDxCHJHYIdBFRKdzkQxZHGBqMNF.jpg');
 /*!40000 ALTER TABLE `ekitaldiak` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,13 +129,13 @@ DROP TABLE IF EXISTS `erabiltzailea`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `erabiltzailea` (
   `id_erabiltzailea` int NOT NULL AUTO_INCREMENT,
-  `izena` varchar(40) DEFAULT NULL,
-  `abizena` varchar(40) DEFAULT NULL,
-  `emaila` varchar(40) DEFAULT NULL,
-  `pasahitza` varchar(50) DEFAULT NULL,
-  `mota` varchar(50) DEFAULT NULL,
+  `izena` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `abizena` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `emaila` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pasahitza` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mota` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_erabiltzailea`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +144,32 @@ CREATE TABLE `erabiltzailea` (
 
 LOCK TABLES `erabiltzailea` WRITE;
 /*!40000 ALTER TABLE `erabiltzailea` DISABLE KEYS */;
+INSERT INTO `erabiltzailea` VALUES (1,'Haritz','Via','via@gmail.com','12345',NULL),(2,'Igor','Viyuela','viyu@gmail.com','12345',NULL);
 /*!40000 ALTER TABLE `erabiltzailea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `eserleku`
+--
+
+DROP TABLE IF EXISTS `eserleku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `eserleku` (
+  `id_eserleku` int NOT NULL,
+  `deskribapena` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id_eserleku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eserleku`
+--
+
+LOCK TABLES `eserleku` WRITE;
+/*!40000 ALTER TABLE `eserleku` DISABLE KEYS */;
+INSERT INTO `eserleku` VALUES (1,'Fila 1, Asiento 1'),(2,'Fila 1, Asiento 2'),(3,'Fila 1, Asiento 3'),(4,'Fila 1, Asiento 4'),(5,'Fila 1, Asiento 5');
+/*!40000 ALTER TABLE `eserleku` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -160,7 +215,7 @@ CREATE TABLE `ikusi` (
   KEY `id_ekitaldia` (`id_ekitaldia`),
   CONSTRAINT `ikusi_ibfk_1` FOREIGN KEY (`id_ekitaldia`) REFERENCES `ekitaldiak` (`id_ekitaldia`),
   CONSTRAINT `ikusi_ibfk_2` FOREIGN KEY (`id_erabiltzailea`) REFERENCES `erabiltzailea` (`id_erabiltzailea`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,10 +296,10 @@ DROP TABLE IF EXISTS `migrations`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `migrations` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -253,7 +308,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'0001_01_01_000000_create_users_table',1),(2,'0001_01_01_000001_create_cache_table',1),(3,'0001_01_01_000002_create_jobs_table',1),(4,'2025_08_26_100418_add_two_factor_columns_to_users_table',1);
+INSERT INTO `migrations` VALUES (5,'0001_01_01_000000_create_users_table',1),(6,'0001_01_01_000001_create_cache_table',1),(7,'0001_01_01_000002_create_jobs_table',1),(8,'2025_08_26_100418_add_two_factor_columns_to_users_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -296,7 +351,7 @@ CREATE TABLE `sarrerak` (
   PRIMARY KEY (`id_sarrerak`),
   KEY `fk_id_ekitaldia` (`fk_id_ekitaldia`),
   CONSTRAINT `sarrerak_ibfk_1` FOREIGN KEY (`fk_id_ekitaldia`) REFERENCES `ekitaldiak` (`id_ekitaldia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,7 +389,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('4H9KPJENKLgTcXxQoWRPMmSQafn9RipRjFaGzeoO',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiUEo4UjdkV1ZBYTBtbE1pd0FNNUh2NjdoMHh6MmtOWU5iUjFPSlQ5QSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1761045044),('B3WAEodymy3MIreUo3eW0YGXCp2OJlexsqNW7Oxh',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoib3RMek1tRWw5elVva25wUmhMNUliQmk4Z1pkSmxnNlFXc08wZEVBWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1761120694);
+INSERT INTO `sessions` VALUES ('BIWA1nbVZRQPZaDlgKMAQ0VWTSqbDKDpSmMDufNY',16,'127.0.0.1','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36','YTo0OntzOjY6Il90b2tlbiI7czo0MDoiSG8zYnRxdkZNN3B1NkpGcWpJWXFsRTFtZzFmMEZ3bDVjb1M1MmZROSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE2O30=',1762947392),('FIz1Z2oamE2Exem3D5mPhXaZZLiqmNxv80bfsjuT',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiMEcxVGpxM1U3SUt2bk9pbVFVSjJiYktiQXlNck9SWG1MbndlMHB3ciI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1762860929),('Owkdrm354RlDJPTMnTBWLJpJVEafmm0QMHQN6u0J',NULL,'127.0.0.1','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36','YTo0OntzOjY6Il90b2tlbiI7czo0MDoiVk5vR1ZCeGJXVjBQQXVubVd5U3MySmpNalYyZzhjVzcxQXhnZk1NcyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6MzoidXJsIjthOjE6e3M6ODoiaW50ZW5kZWQiO3M6MzE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9sYW5naWxlYWsiO319',1762776145),('ytJPlbc68EgfVO6dBj1o5RS2HvIHhTnsTGjxgY8z',NULL,'127.0.0.1','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiam0xRmxDYzBrcVkwZEFRR1dHZlJRZEpVYnRoalN3SWNzb2RZdDlMaiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fX0=',1762945945),('YvvffJUVMqTXiQIZ3DxrVpkp4PGArzDisrbPRR9G',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiY3JSMGNMMUl0N0x3Z1RWRE5ya1Nzd2dlVHBWWmxvSktBYnpsTGRVTyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1762890950);
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -349,6 +404,7 @@ CREATE TABLE `users` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('user','admin','adminNagusia') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user',
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `two_factor_secret` text COLLATE utf8mb4_unicode_ci,
@@ -359,7 +415,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -368,6 +424,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (14,'Admin Nagusia User','admin@gmail.com','adminNagusia',NULL,'$2y$12$gp6mRcxADPBaO5LzP9/6JutTcwdtWqCJSGdPU4o4z69TgKgEPy.Eq',NULL,NULL,NULL,NULL,'2025-11-09 16:17:18','2025-11-09 16:17:18'),(16,'Haritz','haritz@gmail.com','adminNagusia',NULL,'$2y$12$arWbQbY8hVud9Ur1Ob3CzOC4.7hXBB0cJg9IrW6Nc6LhtLfTORby2',NULL,NULL,NULL,NULL,'2025-11-09 16:21:39','2025-11-12 10:02:23');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -380,4 +437,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-22 10:14:09
+-- Dump completed on 2025-11-12 18:44:57
